494a996325ffa79f2154fc079e1e9e5e44e1a162
MCOMMAE <mcommae@alumno.upv.es@ldsic-vdi01.upvnet.upv.es>
2023-10-11 11:25:39 +0200

    Añadida

--- List.h
+++ List.h
@@ -2,17 +2,16 @@
 #define LIST_H
 
 template <typename T> 
-class List {
-    public:
-        virtual void insert(int pos, T e);
-	virtual void append(T e);
-	virtual void prepend(T e);
-	virtual T remove(int pos);
-	virtual T get(int pos);
-	virtual int search(T e);
-	virtual bool empty();
-	int size();
-
+class List{
+public:
+        virtual void insert(int pos, T e) = 0;
+	virtual void append(T e) = 0;
+	virtual void prepend(T e) = 0;
+	virtual T remove(int pos) = 0;
+	virtual T get(int pos) = 0;
+	virtual int search(T e) = 0;
+	virtual bool empty() = 0;
+	virtual int size() = 0;
+      	
 };
-
 #endif
